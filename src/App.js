7e1e152a7f13479f1f1d3ae46658ef3a6df4f8a6
KlimekM <mklimek15@gmail.com>
2021-01-29 22:06:55 -0600

    Add album cover, play clip on hover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
+import albumCover from './images/album-cover.png';
+import treezyClip from './media/treezy.mp3';
 
 function App() {
+  const audioPlayerRef = useRef(null);
+
+  const handleAlbumCoverEnter = () => {
+    audioPlayerRef.current.play();
+  };
+
+  const handleAlbumCoverLeave = () => {
+    audioPlayerRef.current.pause();
+    audioPlayerRef.current.currentTime = 0;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img
+        src={albumCover}
+        alt="Album Cover"
+        className="album-cover"
+        onMouseEnter={handleAlbumCoverEnter}
+        onTouchStart={handleAlbumCoverEnter}
+        onMouseLeave={handleAlbumCoverLeave}
+        onTouchEnd={handleAlbumCoverLeave}
+      />
+      <audio ref={audioPlayerRef} src={treezyClip} />
     </div>
   );
 }
